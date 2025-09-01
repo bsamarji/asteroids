8108bbef0dbabb6ba8c2f5a14934f930ed3c9d5f
bsamarji <bensamarji5637@gmail.com>
2025-09-01 23:30:48 +0200

    Added player sprite and draw on screen.

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     fps = pygame.time.Clock()
     dt = 0
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2, radius=PLAYER_RADIUS)
 
     game_loop = 0
-
     while game_loop == 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = fps.tick(60)
         dt = dt / 1000
