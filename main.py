3ec6e698078ed827d52fb10f5267b05d9f3646a6
bsamarji <bensamarji5637@gmail.com>
2025-09-02 09:53:02 +0200

    Added asteroids and asteroid spawning mechanism to the game loop.

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame
 from constants import *
 from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
+    # initialising the game
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
-    dt = 0
+    dt = 0 # delta time
 
+    # setting up groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    # adding classes to respective groups
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    # creating the player object
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2, radius=PLAYER_RADIUS)
 
+    asteroid_field = AsteroidField()
+
+    # the main game loop
     game_loop = 0
     while game_loop == 0:
         for event in pygame.event.get():
