fbfbd408c182678be271ce0750f9c1a4ff13466f
bsamarji <bensamarji5637@gmail.com>
2025-09-02 09:06:45 +0200

    Added player rotation to the left and right.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
                 return
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = fps.tick(60)
         dt = dt / 1000
