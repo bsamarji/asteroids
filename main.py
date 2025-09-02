a85c1503042ac876a276e9dcc003a6fb6c4cffd8
bsamarji <bensamarji5637@gmail.com>
2025-09-02 09:27:55 +0200

    Swapped to using groups to render updatables and drawables.

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2, radius=PLAYER_RADIUS)
 
     game_loop = 0
@@ -15,8 +19,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for image in drawable:
+            image.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = fps.tick(60)
         dt = dt / 1000
